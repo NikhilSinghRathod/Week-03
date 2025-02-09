ba8e66cffcd8685cc2e05566c76a5af05a9f6f06
NikhilSinghRathod <r7445580@gmail.com>
2025-02-09 20:29:36 +0530

    [Nikhil Singh Rathode] Add. Program of Comparison between linear and Binary Search

---
+++ src/main/java/com/tit/day06runtimeanalysisalgorithm/searchtarget/BinarySearch.java
@@ -0,0 +1,20 @@
+package com.tit.day06runtimeanalysisalgorithm.searchtarget;
+
+public class BinarySearch {
+    // Method for Binary Search
+    public int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // Target not found
+    }
+}
---
+++ src/main/java/com/tit/day06runtimeanalysisalgorithm/searchtarget/LinearSearch.java
@@ -0,0 +1,13 @@
+package com.tit.day06runtimeanalysisalgorithm.searchtarget;
+
+public class LinearSearch {
+    // Method for Linear Search
+    public int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1; // Target not found
+    }
+}
